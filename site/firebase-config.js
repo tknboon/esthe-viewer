51ed0fddb4729309882f38b5c00905368045ec17
tknboon <tknboon@gmail.com>
2026-05-07 15:11:02 +0900

    manual update 2026-05-07 15:10

---
+++ site/firebase-config.js
@@ -0,0 +1,19 @@
+window.firebaseAppConfig = {
+  // Firebase 共有を有効にする
+  enabled: true,
+
+  // Firebase コンソールで表示された Web アプリの設定値
+  apiKey: "AIzaSyCbmczCLf6I5QGnitzdWGCmPh1ksyCJwWw",
+  authDomain: "esthe-viewer.firebaseapp.com",
+  projectId: "esthe-viewer",
+  storageBucket: "esthe-viewer.firebasestorage.app",
+  messagingSenderId: "972599031473",
+  appId: "1:972599031473:web:d8fb490359f6af36025490",
+
+  // 空のままなら、ログインした人は全員編集できます。
+  // ここにメールアドレスを入れると、その人だけ編集できます。
+  editorEmails: ["tknboon@gmail.com"],
+
+  // UID で絞りたいときはここも使えます。
+  editorUids: [],
+};
